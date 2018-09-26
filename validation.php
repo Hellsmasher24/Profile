18fcd0cc2990b0b548873e2fa0793d03e8aab33d
Hellsmasher24 <deep.kamani24@gmail.com>
2018-09-26 12:11:31 -0400

    Form channged and created processing script with file upload code

--- validation.php
+++ validation.php
@@ -1,8 +1,24 @@
 <?php
 
+header("Location: photo.php");
+var_dump($_POST);
+
+echo ("Welcome ". $_POST['firstname'+'lastname']. "<br />");
+   echo ("Your ". $_POST['email']. " is registered");
+   echo()
 
 
+   $uploaddir = '/profilePic';
+   $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 
+   if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+       echo "File is valid, and was successfully uploaded.\n";
+   } else {
+       echo "Possible file upload attack!\n";
+   }
+
+   echo 'Here is some more debugging info:';
+   print_r($_FILES);
 
 
 
